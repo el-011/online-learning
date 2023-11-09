34cf230594420aec4ddd954a460a9c9654876e64
w3villa-renu-vishwakarma <renu.vishwakarma@w3villa.com>
2023-11-09 13:56:11 +0530

    home UI added

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,14 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+const navBar = document.getElementById("topnav");
+const closeMenu = document.getElementById("close");
+const menuBox = document.getElementById("menu");
+
+navBar.addEventListener("click", function () {
+  menuBox.classList.add("responsive");
+});
+closeMenu.addEventListener("click", function () {
+  menuBox.classList.remove("responsive");
+});
